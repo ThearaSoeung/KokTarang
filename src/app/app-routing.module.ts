4948c2c78d01c405f957a78026056f1b2414cf4c
cheat2001 <chansocheatsok2001@gmail.com>
2022-11-21 11:11:26 +0700

    add card to about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HelpCenterComponent } from './pages/help-center/help-center.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { OpponentsComponent } from './pages/opponents/opponents.component';
+import { PagesComponent } from './pages/pages.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { TarangComponent } from './pages/tarang/tarang.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'support', component: HelpCenterComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'page', component: PagesComponent },
+  { path: 'opponents', component: OpponentsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -11,5 +11,42 @@ export class AboutUsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  developer: any = [
+    {
+      name: "Bro Ro",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://images.teamtalk.com/content/uploads/2022/10/31115902/man-utd-striker-cristiano-ronaldo.jpg'
+    },
+    {
+      name: "Bro Ra",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://media.cnn.com/api/v1/images/stellar/prod/220605164634-messi-estonia-5-goals.jpg?c=original'
+    },
+    {
+      name: "Bro Ro",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://images.teamtalk.com/content/uploads/2022/10/31115902/man-utd-striker-cristiano-ronaldo.jpg'
+    },
+    {
+      name: "Bro Ro",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://images.teamtalk.com/content/uploads/2022/10/31115902/man-utd-striker-cristiano-ronaldo.jpg'
+    },
+    {
+      name: "Bro Ro",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://images.teamtalk.com/content/uploads/2022/10/31115902/man-utd-striker-cristiano-ronaldo.jpg'
+    },
+    {
+      name: "Bro Ro",
+      position: "Designer",
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sed velit nobis molestiae',
+      img: 'https://images.teamtalk.com/content/uploads/2022/10/31115902/man-utd-striker-cristiano-ronaldo.jpg'
+    },
+  ]
 }
