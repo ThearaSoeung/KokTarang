f16f45a010d4632f4e36e8c1b53e57dfbc851429
Ewent Neak <ewentfirex@gmail.com>
2022-11-21 15:22:09 +0700

    finish homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  //image
+  imageSport: string = "assets/sportall.png";
+  tickImage: string = "assets/tick.png";
+  abaLogo: string = "assets/logos/aba-web-top-logo.png";
+  nham24Logo: string = "assets/logos/logo.png";
+  aeonMallLogo: string = "assets/logos/unnamed.png";
+  legnendCinemaLogo: string = "assets/logos/legend_logo.png";
+  cellcardLogo: string = "assets/logos/logo-for-reach-02-1.png";
 
 }
